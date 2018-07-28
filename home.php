50e6c7af6afede405715f0a10cc4aafd737eb001
fatkhuliyan <37074414+fatkhuliyan@users.noreply.github.com>
2018-07-28 15:14:37 +0700

    Create home.php

---
+++ home.php
@@ -0,0 +1,162 @@
+<!--main content start-->
+<section id="main-content">
+  <section class="wrapper">
+    <div class="row">
+        <div class="col-lg-9 main-chart">
+          <div class="row mt">
+
+            <div class="slideshow-container">
+
+            <div class="mySlides fade_">
+              <div class="numbertext">1 / 3</div>
+              <img src="<?php echo base_url(); ?>assets/img/H0007.JPG" style="width:100%">
+              <div class="text_">Gambar 1</div>
+            </div>
+
+            <div class="mySlides fade_">
+              <div class="numbertext">2 / 3</div>
+              <img src="<?php echo base_url(); ?>assets/img/H0007.JPG" style="width:100%">
+              <div class="text_">Caption Two</div>
+            </div>
+
+            <div class="mySlides fade_">
+              <div class="numbertext">3 / 3</div>
+              <img src="<?php echo base_url(); ?>assets/img/H0007.JPG" style="width:100%">
+              <div class="text_">Caption Three</div>
+            </div>
+
+            <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+            <a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+            </div>
+            <br>
+
+            <div style="text-align:center">
+              <span class="dot" onclick="currentSlide(1)"></span> 
+              <span class="dot" onclick="currentSlide(2)"></span> 
+              <span class="dot" onclick="currentSlide(3)"></span> 
+            </div>
+
+    	<div class="col-md-4 col-sm-4 mb">
+    	    <div class="white-panel pn">
+        	  	<div class="white-header">
+            	 <h5>PLASMA 6</h5>
+            	</div>
+          		<div class="row">
+            		<div class="col-sm-6 col-xs-6 goleft">
+              			<p><i class="fa fa-dollar"></i> 532.000</p>
+            		</div>
+            		<div class="col-sm-6 col-xs-6"></div>
+          		</div>
+          		<div class="centered">
+            		<img src="<?php echo base_url(); ?>assets/img/H0007.JPG" width="200">
+          		</div>
+        	</div>
+    	</div><!-- /col-md-4-->
+    	
+    	<div class="col-md-4 col-sm-4 mb">
+    		<div class="white-panel pn">
+    			<div class="white-header">
+			           <h5>PLASMA 99</h5>
+    			</div>
+	            <div class="row">
+            		<div class="col-sm-6 col-xs-6 goleft">
+              			<p><i class="fa fa-dollar"></i> 752.000</p>
+            		</div>
+            		<div class="col-sm-6 col-xs-6"></div>
+      			</div>
+      			<div class="centered">
+            		<img src="<?php echo base_url(); ?>assets/img/H0008.JPG" width="200">
+      			</div>
+    		</div>
+    	</div><!-- /col-md-4 -->
+    	
+		<div class="col-md-4 col-sm-4 mb">
+        	<div class="white-panel pn">
+          		<div class="white-header">
+             		<h5>RAK SEPATU 120</h5>
+          		</div>
+          		<div class="row">
+            		<div class="col-sm-6 col-xs-6 goleft">
+              			<p><i class="fa fa-dollar"></i> 450.000</p>
+            		</div>
+            		<div class="col-sm-6 col-xs-6"></div>
+          		</div>
+          		<div class="centered">
+            		<img src="<?php echo base_url(); ?>assets/img/H0006.JPG" width="120">
+          		</div>
+        	</div>
+     	</div><!-- /col-md-4 -->
+    </div><!-- /row -->
+   
+   	<div class="row">
+		<div class="col-md-4 mb">
+            <div class="white-panel pn">
+                <div class="white-header">
+                   <h5>LP 992 SAKURA</h5>
+                </div>
+                <div class="row">
+                	<div class="col-sm-6 col-xs-6 goleft">
+                    	<p><i class="fa fa-dollar"></i> 698.000</p>
+                  	</div>
+                	<div class="col-sm-6 col-xs-6"></div>
+                </div>
+                <div class="centered">
+                  	<img src="<?php echo base_url(); ?>assets/img/H0001.JPG" width="120">
+                </div>
+             </div>
+		</div><!-- /col-md-4 -->
+						
+						
+		<div class="col-md-4 mb">
+    	    <div class="white-panel pn">
+        	    <div class="white-header">
+            	   <h5>LEMARI MAKAN 2</h5>
+            	</div>
+            	<div class="row">
+              		<div class="col-sm-6 col-xs-6 goleft">
+                		<p><i class="fa fa-dollar"></i> 752.500</p>
+              		</div>
+              		<div class="col-sm-6 col-xs-6"></div>
+            	</div>
+            	<div class="centered">
+              		<img src="<?php echo base_url(); ?>assets/img/H0002.JPG" width="120">
+            	</div>
+          </div>
+      	</div><!-- /col-md-4 -->
+						
+		<div class="col-md-4 mb">
+            <div class="white-panel pn">
+                <div class="white-header">
+                   <h5>MEJA RIAS 120</h5>
+                </div>
+                <div class="row">
+                  <div class="col-sm-6 col-xs-6 goleft">
+                    <p><i class="fa fa-dollar"></i> 757.500</p>
+                  </div>
+                  <div class="col-sm-6 col-xs-6"></div>
+                </div>
+                <div class="centered">
+                  <img src="<?php echo base_url(); ?>assets/img/H0004.JPG" width="120">
+                </div>
+              </div>
+          </div><!-- /col-md-4 -->
+
+          <div class="col-md-4 mb">
+            <div class="white-panel pn">
+                <div class="white-header">
+                   <h5>KITCHEN SET ATAS 3</h5>
+                </div>
+                <div class="row">
+                  <div class="col-sm-6 col-xs-6 goleft">
+                    <p><i class="fa fa-dollar"></i> 950.500</p>
+                  </div>
+                  <div class="col-sm-6 col-xs-6"></div>
+                </div>
+                <div class="centered">
+                  <img src="<?php echo base_url(); ?>assets/img/H0005.JPG" width="200">
+                </div>
+              </div>
+          </div><!-- /col-md-4 -->
+			</div><!-- /row -->
+  </div>
